3eb278c559716040f48cb4ed415128904e7b90ce
Víctor Trevijano Ramos <vtrevijano@minsait.com>
2022-01-18 08:21:37 +0100

    basic job done, maps not yet

--- prácticas/js/info.js
+++ prácticas/js/info.js
@@ -1,14 +1,9 @@
 "use strict";
+import { paintPokemon } from "./list.js";
 
-//PETICIÓN A LA API DE POKEAPI PARA DEVOLVER LOS 10 PRIMEROS POKEMON.
-
-const totalPokemon = document.querySelector(".character__list");
-
+//PETICIÓN A LA API DE POKEAPI PARA DEVOLVER LOS 9 PRIMEROS POKEMON.
 const getAllPokemons = async () => {
-
-  //PETICIÓN A LA POKEAPI
-
-  const pokemonList = await fetch(
+const pokemonList = await fetch(
     "https://pokeapi.co/api/v2/pokemon?offset=0&limit=9"
   );
   const pokemonsToJson = await pokemonList.json();
@@ -24,36 +19,9 @@ const getAllPokemons = async () => {
   }));
 
   //PASAMOS A LA FUNCIÓN QUE PINTA EL LISTADO LA INFO DE LA CONSTANTE
-
   paintPokemon(pokemonInfo);
 
 };
 
-const paintPokemon = (pokemons) => {
-
-    //LA INFO QUE SE LE PASE A PAINTPOKEMON SE MAPEARÁ
-
-  const totalInfo = pokemons.map(
-      (pokemon) =>
-        `<li onclick="Swal.fire({
-            title: '${pokemon.name}',
-            text: 'El id es: ${pokemon.id}',
-            imageUrl: '${pokemon.image}',
-            imageWidth: 280,
-            background: '#213a65',
-            imageAlt: 'Pokemon Image',
-            padding: '1rem 0',
-            color: '#feca1b',
-          })";
-            class="pokemon__list"/>
-
-        <h1 class="pokemon__name">${pokemon.name}</h1>
-        <img src="${pokemon.image}" alt="${pokemon.name}"/>
-        </li>`
-    )
-    .join("");
-
-    //LA INFO SE AÑADE AL CONTENIDO HTML DE LA UL
-  totalPokemon.innerHTML = totalInfo;
-};
+ 
 export { getAllPokemons };
--- prácticas/js/list.js
+++ prácticas/js/list.js
@@ -1,16 +1,28 @@
-/* import { getAllPokemons } from "./info.js";
+const totalPokemon = document.querySelector(".character__list");
 
-const paintPokemon = pokemons => {
+//LA INFO QUE SE LE PASE A PAINTPOKEMON SE MAPEARÁ
+const paintPokemon = (pokemons) => {
     const totalInfo = pokemons.map(
         (pokemon) =>
-        `<li class="pokeList">
-        <h1>${pokemon.name}</h1>
-        <img src="${pokemon.image}" alt="${pokemon.name}"/>
-        <button class="detail__button">Ver detalle</button>
-        </li>`
-    ).join('');
-    totalPokemon.innerHTML = totalInfo
-    window.addEventListener('load', getAllPokemons)
-}
-
-export {paintPokemon} */
+          `<li onclick="Swal.fire({
+              title: '${pokemon.name}',
+              text: 'El id es: ${pokemon.id}',
+              imageUrl: '${pokemon.image}',
+              imageWidth: 280,
+              background: '#213a65',
+              imageAlt: 'Pokemon Image',
+              padding: '1rem 0',
+              color: '#feca1b',
+            })";
+              class="pokemon__list"/>
+  
+          <h1 class="pokemon__name">${pokemon.name}</h1>
+          <img src="${pokemon.image}" alt="${pokemon.name}"/>
+          </li>`
+      )
+      .join("");
+  
+      //LA INFO SE AÑADE AL CONTENIDO HTML DE LA UL
+    totalPokemon.innerHTML = totalInfo;
+  };
+  export {paintPokemon}
